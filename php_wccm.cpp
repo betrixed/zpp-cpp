a14c577c9531f1388967d355f3268d9937981e91
Michael Rynn <michael.rynn.500@gmail.com>
2026-02-16 17:59:01 +1100

    delete marktohtml code

--- php_wccm.cpp
+++ php_wccm.cpp
@@ -4,8 +4,9 @@
 #include "php_wccm.h"
 #include "zpp/show_zpp.h"
 
-#include "md4c/markhtml.cpp"
+
 #include "toml/toml_php.cpp"
+
 
 // CacheMgr wants one of these
 #include "tinyxml/dxmlread.cpp"
@@ -15,18 +16,17 @@ zpp::state_list  STATE_LIST_NAME("wccm");
 PHP_MINIT_FUNCTION(wccm)
 {	
 
+
 #ifdef DAYTIME_CPP
 	PHP_MINIT(Wcc_Day24_reg)(INIT_FUNC_ARGS_PASSTHRU);
 #endif
 
+
 #ifdef TOML_PHP_CPP
 	PHP_MINIT(Wcc_Toml_reg)(INIT_FUNC_ARGS_PASSTHRU);
 #endif
-
-#  ifdef MARKHTML_CPP
-	PHP_MINIT(Wcc_MarkToHtml_reg)(INIT_FUNC_ARGS_PASSTHRU);
-#  endif
 	
+
 
 #	ifdef DXMLREAD_CPP
 	PHP_MINIT(Wcc_XmlRead_reg)(INIT_FUNC_ARGS_PASSTHRU);
