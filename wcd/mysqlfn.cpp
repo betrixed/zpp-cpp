29d11ffcb2696598f22fe322d97b15ddfcb05c40
Michael Rynn <michael.rynn.500@gmail.com>
2026-03-23 00:35:58 +1100

    mysqlfn driver

--- wcd/mysqlfn.cpp
+++ wcd/mysqlfn.cpp
@@ -15,7 +15,6 @@ namespace wcd {
 
 using namespace zpp;
 
-base_obj_mgr<Mysqlfn> Mysqlfn::omg;
 
 class MSFInit : public state_init {
 public:
@@ -36,18 +35,41 @@ public:
 	str_intern rollback_commit_s;
 
 	void init() override;
+	void init_req() override;
+
 };
+
+class MsFnTable {
+public:
+	fn_call         fetch_array_fn;
+	fn_call			set_charset_fn;
+	fn_call			query_fn;
+	fn_call         report_fn;
+	
+
+	initFn(MSFInit& ms)
+	{
+		fetch_array_fn.set_fci(ms.fetch_array_s);
+		query_fn.set_fci(ms.query_s);
+		report_fn.set_fci(ms.report_mode_s);
+		set_charset_fn.set_fci(ms.set_charset_s);
+	}
+};
+
+base_obj_mgr<Mysqlfn> Mysqlfn::omg;
+MFSInit Msfi;
 
 void
 MSFInit::init()
 {
 	mysqli_s = "mysqli";
-	report_mode_s = "report_mode";
-	query_s = "query";
-	prepare_s = "prepare";
-	set_charset_s = "set_charset";
+	report_mode_s = "mysqli_report";
+	prepare_s = "mysqli_prepare";
+	query_s = "mysqli_query";
 
-	fetch_array_s = "fetch_array";
+	set_charset_s = "mysqli_set_charset";
+
+	fetch_array_s = "mysqli_fetch_array";
 	fetch_object_s = "fetch_object";
 	fetch_assoc_s = "fetch_assoc";
 	fetch_all_s = "fetch_all";
@@ -72,48 +94,50 @@ Mysqlfn::~Mysqlfn()
 	}
 }
 
-MSFInit  Msfi;
+thread_local MsFnTable MSfn;
 
-class MsiWrap {
-protected:
-	obj_rc  		handle_;
-	fn_call	set_charset_fn;
-	fn_call	query_fn;
-public:
 
-	MsiWrap(obj_ptr h);
-
-	void set_charset(str_ptr cset);
-	val_rc query(str_ptr query, int result_mode = MSQLI_STORE_RESULT);
-
-};
-
-MsiWrap::MsiWrap(obj_ptr h) 
+bool 
+mysqli_set_charset(obj_ptr msi, str_ptr cset)
 {
-	set_charset_fn.set_fci(h,Msfi.set_charset_s);
-	query_fn.set_fci(h,Msfi.query_s);
+	fn_params  fn(MSfn.set_charset_fn);
+	zval* pz = fn.argsptr();
+	val_ptr::object_bind(pz, msi);
+	pz++; val_ptr::string_bind(pz, cset);
+	return fn.zbool();
 }
 
-void 
-MsiWrap::set_charset(str_ptr cset)
+obj_rc
+mysqli_query(obj_ptr msi, int rmode)
 {
-	auto& fn = set_charset_fn;
-	zval* zp = fn.argsptr();
-	val_ptr::string_bind(zp, cset);
-	fn.call_fn();
+	fn_params  fn(MSfn.query_fn);
+	zval* pz = fn.argsptr();
+	val_ptr::object_bind(pz, msi);
+	pz++; ZVAL_LONG(pz, rmode);
+	return fn.obj();
 }
 
-val_rc 
-MsiWrap::query(str_ptr query, int result_mode = MSQLI_STORE_RESULT)
+
+bool 
+mysqli_report(int flags)
 {
-	auto& fn = query_fn;
-	zval* zp = fn.argsptr();
-	val_ptr::string_bind(zp, query);
-	zp++;
-	ZVAL_LONG(zp,result_mode);
-	val_rc result = fn.call_fn();
-	return result;
+	fn_params  fn(MSfn.report_fn);
+	zval* pz = fn.argsptr();
+	ZVAL_LONG(pz, flags);
+	return fn.zbool();
 }
+
+htab_rc 
+mysqli_fetch_array(obj_ptr robj, int mode)
+{
+	fn_params  fn(MSfn.fetch_array_fn);
+	zval* pz = fn.argsptr();
+	val_ptr::object_bind(pz, robj);
+	pz++;
+	ZVAL_LONG(pz, mode);
+	return fn.zbool();
+}
+
 
 void 
 register_class(zend_class_entry* idriver_ce)
@@ -187,6 +211,8 @@ commit()
 {
 	
 }
+
+
 
 error_return 
 connect()
--- wcd/mysqlfn.h
+++ wcd/mysqlfn.h
@@ -74,19 +74,5 @@ public:
 };
 
 
-class MsiWrap {
-protected:
-	obj_rc  		handle_;
-	fn_call_args1	set_charset_fn;
-	fn_call_args2	query_fn;
-public:
-
-	MsiWrap(obj_ptr h);
-
-	void set_charset(str_ptr cset);
-	val_rc query(str_ptr query, int result_mode = MSQLI_STORE_RESULT);
-
-};
-
 
 #endif
