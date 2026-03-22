06f62c21641a3ee5143272700f7b17a9dc7e1633
Michael Rynn <michael.rynn.500@gmail.com>
2026-03-22 21:09:33 +1100

    build on archarm

--- pvid.sh
+++ pvid.sh
@@ -1,14 +1,7 @@
 #!/bin/bash
 . /etc/os-release
-if [ $ID == "debian" ]; then
-  pvid=`php -r "echo(PHP_MAJOR_VERSION . '.' . PHP_MINOR_VERSION);"`
-elif [[ $ID == "arch" || $ID == "endeavouros" ]]; then
-#  pvid=""
-   pvid=`php -r "echo(PHP_MAJOR_VERSION . PHP_MINOR_VERSION);"`
-elif [[ $ID == "archarm" ]]; then
-   pvid=""
-else
-  pvid="Unknown case for $ID"	
-fi
+pconf=`php-config --ini-dir`
+pvid=`php -r "echo(PHP_MAJOR_VERSION . '.' . PHP_MINOR_VERSION);"`
+echo $pconf
 echo $pvid
 # sudo systemctl restart php${pvid}-fpm
